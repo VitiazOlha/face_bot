1452740a4746a502b988df042ca38fa62326177c
Vitiaz Olha <vitiazolha@gmail.com>
2017-07-07 15:11:00 +0300

    get and post request added

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,29 +1,71 @@
 package controllers
 
-import play.api._
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsNumber, JsValue, Json}
+import play.api.libs.ws.{WS, WSResponse}
 import play.api.mvc._
+
+import scala.concurrent.Future
 
 object Application extends Controller {
 
+  val PAT = "EAAESnta9mIwBANXgf6NVOSDsfujMwjM60kf778WGXwAKTbB0CdaZBQL2m10sHPODJq6IOiineaYBjxdGhqnyr6h3DkMOIZBEqQo3xOOouUnqy0YZCfiy3b6H6I1MXoptxvxnkmGRInneOZC7A4kGZB4vZAaF1ZCSkDv4mZAMuex1tQZDZD"
+
   def index = Action { request =>
-//    if (request.getQueryString("hub.verify_token").get == "vohello")
+    if (request.getQueryString("hub.verify_token").get == "vohello")
       Ok(request.getQueryString("hub.challenge").get)
-//    else
-//      Ok("Error")
+    else
+      Ok("Error")
   }
 
-  def postQuery: Action[JsValue] = Action (parse.json) { request =>
-//    val json = request.body.asJson.get
-    (request.body \ "recipient_id").asOpt[String].map { recipient_id =>
-      (request.body \ "message_id").asOpt[String].map { message_id =>
-        Ok(Json.toJson(
-          Map(
-            "recipient" -> Json.toJson(Map("id" -> "recipient_id")),
-            "message" -> Json.toJson(Map("text" -> "hello")))
-        ))
-      }.getOrElse(Ok("Error"))
-    }.getOrElse(Ok("Error"))
+  def getMessage(text: String): JsValue = text match {
+    case "hello" => Json.toJson(Map("text" -> "Hello!"))
+    case "help" => Json.toJson(Map("text" -> "Hello, i`m TProger bot.\n@\"tag\" - subscribe on news\n#\"tag\" - get all news about some teg"))
+    case a if a.charAt(0) == '@' => Json.toJson(Map("text" -> "sub"))
+    case a if a.charAt(0) == '#' => Json.toJson(Map("text" -> "get"))
+    case _ => Json.toJson(Map("text" -> "Unknown command"))
   }
+
+  def sendMessege(sender: BigDecimal, message: JsValue): Future[WSResponse] = {
+    WS.url(s"https://graph.facebook.com/v2.6/me/messages?access_token=$PAT")
+      .withHeaders("Content-Type" -> "application/json")
+      .withRequestTimeout(1000)
+      .post(Json.toJson(Map("recipient" -> Json.toJson(Map("id" -> JsNumber(sender))), "message" -> message)))
+  }
+
+  def postQuery: Action[JsValue] = Action.async(parse.json) { request =>
+    val json = Json.toJson(request.body)
+    val text = (((json \ "entry") (0) \ "messaging") (0) \ "message" \ "text").as[String]
+    val sender = (((json \ "entry") (0) \ "messaging") (0) \ "sender" \ "id").as[BigDecimal]
+    val message = getMessage(text)
+
+    val res = sendMessege(sender, message)
+
+    res.map { jsResponse =>
+      Ok(jsResponse.body)
+    }
+  }
+
+  /*
+  def db = Action {
+    var out = ""
+    val conn = DB.getConnection()
+    try {
+      val stmt = conn.createStatement
+
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
+
+      val rs = stmt.executeQuery("SELECT tick FROM ticks")
+
+      while (rs.next) {
+        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
+      }
+    } finally {
+      conn.close()
+    }
+    Ok(out)
+  }
+  */
+
 }
 
--- build.sbt
+++ build.sbt
@@ -6,8 +6,12 @@ lazy val `tprober_bot` = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.11.7"
 
-libraryDependencies ++= Seq(jdbc, cache, ws, specs2 % Test)
+libraryDependencies ++= Seq(jdbc, cache, "org.postgresql" % "postgresql" % "9.4-1201-jdbc41", ws, specs2 % Test)
 
 unmanagedResourceDirectories in Test <+= baseDirectory(_ / "target/web/public/test")
 
-resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"  
+resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+
+libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _ )
+
+libraryDependencies += "net.ruippeixotog" %% "scala-scraper" % "2.0.0-RC2"
