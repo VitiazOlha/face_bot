4104e60ce1d7635bd890c34b43d93fd73499782d
Vitiaz Olha <vitiazolha@gmail.com>
2017-07-09 00:18:21 +0300

    db fix

--- app/Global.scala
+++ app/Global.scala
@@ -3,6 +3,6 @@ import play.api.{Logger, _}
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
-    models.Subscription.init
+//    models.Subscription.init
   }
 }
